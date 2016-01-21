546d8c3f6b7a63307e342d18359f9e53005d72b3
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-21 17:24:00 -0500

    experiment with overriding bitwise operators

--- flow/api.py
+++ flow/api.py
@@ -1,72 +1,111 @@
 
+from __future__ import absolute_import
+
+import traits.api as T
+from traits.api import HasTraits
+
 import networkx as nx
 import time
 from functools import wraps
 from concurrent import futures
 from multiprocessing import cpu_count
 
-_DEFAULT_EXECUTOR = futures.ThreadPoolExecutor(cpu_count())
-_FLOW_NAMESPACE = None
 
-def namespace_clear():
-    global _FLOW_NAMESPACE
-    _FLOW_NAMESPACE = dict()
+Graph = nx.DiGraph
 
-def namespace_init(name):
-    global _FLOW_NAMESPACE
-    _FLOW_NAMESPACE[name] = nx.DiGraph()
+DiGraphT = T.Trait(nx.DiGraph)
+ExecutorT = T.Trait(futures.Executor)
 
-def namespace_use(name):
-    global _FLOW_NAMESPACE
-    if name not in _FLOW_NAMESPACE:
-        namespace_init(name)
 
-def namespace_get(name):
-    global _FLOW_NAMESPACE
-    return _FLOW_NAMESPACE[name]
+class Task(HasTraits):
+    G = DiGraphT()
+    executor = ExecutorT()
+    name = T.String()
 
-class task(object):
-    def __init__(self, namespace='.', executor=None):
-        namespace_use(namespace)
-        self._namespace = namespace
-        self._G = namespace_get(namespace)
+    def __init__(self, fn, graph=None, executor=None):
+        self.function = fn
+        self.name = fn.func_name
 
-        global  _DEFAULT_EXECUTOR
-        self._executor = executor or _DEFAULT_EXECUTOR
+        if graph is not None:
+            self.G = graph
 
-    @property
-    def namespace(self): return self._namespace
+        if executor is not None:
+            self.executor = executor
+    
+    def __call__(self, *args, **kwargs):
+        future = self.executor.submit(self.function, *args, **kwargs)
+        self.G.add_node(self.function.func_name, future=future)
+        return future
 
-    @property
-    def G(self): return self._G
 
-    @property
-    def executor(self): return self._executor
+    def __add__(self, other):
+        assert self.G is not None
+        other.G = self.G
+        return other
 
-    def __call__(self, fn):
+    def __and__(self, other):
 
-        @wraps(fn)
-        def work(*args, **kwargs):
+        # self.G and other.G reference the same object
+        G = self.G
 
-            f = self.executor.submit(fn, *args, **kwargs)
-            self.G.add_node(fn.func_name, future=f)
-            return f
+        # create a boundary
+        if self.name != '--BOUNDARY--' and other.name != '--BOUNDARY--':
+            b = boundary(G)
+            G.add_edge(self.function, b.function)
+            G.add_edge(other.function, b.function)
+            return b
 
-        return work
+        # merge boundaries
+        elif self.name == '--BOUNDARY--' and other.name == '--BOUNDARY--':
+            incomming = G.predecessors(other.function)
+            G.remove_node(other.function)
+            for n in incomming:
+                G.add_edge(n, self.function)
+            return self
 
-namespace_clear()
+        # either self or other is a boundary
+        else:
+            b = self if self.name == '--BOUNDARY--' else other
+            o = self if self.name != '--BOUNDARY--' else other
+            assert b.name == '--BOUNDARY--', b.name
+            assert o.name != '--BOUNDARY--', o.name
+            G.add_edge(o.function, b.function)
+            return b
 
-@task()
+
+
+def boundary(graph):
+    def _boundary(): return
+    t = Task(_boundary, graph=graph)
+    t.name = '--BOUNDARY--'
+    return t
+
+
+def inject(graph, task, **kws):
+    task.G = graph
+    return task
+
+
+def task(fn):
+    return Task(fn)
+
+
+@task
 def A():
     for i in xrange(4):
         # print 'A', i
         time.sleep(5)
 
-@task()
+@task
 def B():
     for i in xrange(3):
         # print 'B', i
         time.sleep(2)
+
+@task
+def C():
+    for i in xrange(2):
+        time.sleep(4)
 
 
 def main():
@@ -77,14 +116,17 @@ def main():
     A()
     B()
 
-main()
-g = namespace_get('.')
 
-while True:
-    for n in g.nodes():
-        print n, g.node[n]['future'].done()
-    if all(map(lambda n: g.node[n]['future'].done(), g.nodes())):
-        break
-    else:
-        time.sleep(2)
-        print
+# main()
+
+# while True:
+#     for n in G.nodes():
+#         print n, G.node[n]['future'].done()
+#     if all(map(lambda n: G.node[n]['future'].done(), G.nodes())):
+#         break
+#     else:
+#         time.sleep(2)
+#         print
+
+
+G = nx.DiGraph()
