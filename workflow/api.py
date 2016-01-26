7bcb6a5ccda369c969c0c0a8c1a97be03a979612
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-26 14:23:56 -0500

    get evaluation to work

--- workflow/api.py
+++ workflow/api.py
@@ -10,16 +10,21 @@ import uuid
 from functools import wraps
 from concurrent import futures
 from multiprocessing import cpu_count
+from collections import OrderedDict
 
+class Graph(nx.DiGraph):
+    """A NetworkX DiGraph where the ordering of edges/nodes is
+    preserved"""
 
-Graph = nx.DiGraph
+    node_dict_factory = OrderedDict
+    adjlist_dict_factory = OrderedDict
+
 
 TDiGraph = T.Trait(nx.DiGraph)
 TExecutor = T.Trait(futures.Executor)
 
 
 def nodeid(): return uuid.uuid4()
-
 
 class Operator:
     AND = '&'
@@ -46,11 +51,12 @@ class delayed(object):
         return g
 
 
-def OpNode(graph, operator):
-    node = Node((lambda:None, (), {}), graph=graph, type=NodeType.OPERATOR)
-    node.id = nodeid()
-    node.f.func_name = operator
-    return node
+
+def evaluate(graph):
+
+    s = nx.topological_sort(graph)[0]
+    node = graph.node[s]['node']
+    return node.eval()
 
 
 class Node(HasTraits):
@@ -60,18 +66,34 @@ class Node(HasTraits):
     executor = T.Trait(futures.ThreadPoolExecutor)
     timeout = T.Any()
     f = T.Function()
+    name = T.String()
     result = T.Trait(futures.Future)
     type = T.String()
     
-    def __init__(self, (f, args, kws), graph=None, executor=None, timeout=None, type=None):
+    def __init__(self, (f, args, kws), graph=None, executor=None,
+                 timeout=None, type=None):
         self.id = nodeid()
         self.f = f
         self._args = args
         self._kws = kws
+        self.name = f.func_name
         self.graph = graph
         self.executor = executor or futures.ThreadPoolExecutor(cpu_count())
         self.timeout = timeout
         self.type = type or NodeType.COMPUTE
+
+
+    @property
+    def children_iter(self):
+        edges = set(self.graph.edges())
+        for i in self.graph.successors_iter(self.id):
+            child = self.graph.node[i]['node']
+            assert (self.id, i) in edges
+            yield child
+
+    @property
+    def children(self):
+        return list(self.children_iter)
 
 
     def start(self):
@@ -82,21 +104,30 @@ class Node(HasTraits):
             pass
 
 
-    def compose(self, other, operator):
+    def wait(self):
+        self.result.result(self.timeout)
+
+
+    def eval(self):
+        self.start()
+        self.wait()
+
+
+    def compose(self, other, MkOpNode):
 
         assert self.graph is not None
         assert other.graph is not None
         assert self.graph == other.graph
         G = self.graph
 
-        print self.f.func_name, operator, other.f.func_name
+        # print self.name, operator, other.name
 
         s, t = self.id, other.id
         other.id = t
-        op = OpNode(G, operator)
-        G.add_node(op.id, node=op, label=operator)
-        G.add_node(s, node=self, label=self.f.func_name)
-        G.add_node(t, node=other, label=other.f.func_name)
+        op = MkOpNode(graph=G)
+        G.add_node(op.id, node=op, label=op.name)
+        G.add_node(s, node=self, label=self.name)
+        G.add_node(t, node=other, label=other.name)
         G.add_edge(op.id, s)
         G.add_edge(op.id, t)
 
@@ -104,39 +135,96 @@ class Node(HasTraits):
 
 
     def __and__(self, other):
-        return self.compose(other, Operator.AND)
+        return self.compose(other, AndNode)
 
 
     def __or__(self, other):
-        return self.compose(other, Operator.OR)
+        return self.compose(other, OrNode)
+
+
+
+class OpNode(Node):
+
+    def __init__(self, **kwargs):
+        n = self.name
+        super(OpNode, self).__init__((lambda: None, (), {}), **kwargs)
+        self.name = n
+
+class AndNode(OpNode):
+
+    name = T.String('&')
+
+    def start(self):
+        for child in self.children_iter:
+            child.start()
+            break
+
+
+    def wait(self):
+        self.result = futures.Future()
+        for child in self.children_iter:
+            child.start()
+            child.wait()
+        self.result.set_result(None)
+
+
+class OrNode(OpNode):
+
+    name = T.String('|')
+
+    def start(self):
+        for child in self.children_iter:
+            child.start()
+
+    def wait(self):
+        for child in self.children_iter:
+            child.wait()
 
 
 G = Graph()
 
 @delayed(G)
 def A():
+    print 'A START'
     # for i in xrange(10):
     #     print 'A', i
-    time.sleep(1)
+    time.sleep(3)
+    # print 'A STOP'
 
 @delayed(G)
 def B():
-    time.sleep(2)
+    print 'B START'
+    time.sleep(3)
+    # print 'B STOP'
 
 @delayed(G)
 def C():
+    print 'C START'
     time.sleep(3)
+    # print 'C STOP'
+
+@delayed(G)
+def D():
+    print 'D START'
+    time.sleep(3)
+    # print 'D STOP'
+
+@delayed(G)
+def F():
+    print 'F START'
+    time.sleep(3)
+    # print 'F STOP'
 
 
 def clean(G):
-    H = nx.convert_node_labels_to_integers(G)
+    H = G.copy() # nx.convert_node_labels_to_integers(G)
     N = {}
     E = {}
 
     for n in H.nodes():
         node = H.node[n]['node']
         del H.node[n]['node']
-        N[n] = node.f.func_name
+        N[n] = node.name
 
         # for s,t,data in H.out_edges([n], data=True):
         #     E[s,t] = data['type']
@@ -144,9 +232,25 @@ def clean(G):
     return H, N, E
 
 def test():
+    # A() | B() | C()
+    # A() & B() | C()
     # A() | (B() & C())
-    ((A() & B()) | C()) & (A() | C())
+    # ((A() & B()) | C()) & (D() | F())
+    (A() | B() | C()) & (D() | F())
+
     H, N, E = clean(G)
+
+
+    evaluate(G)
+
+    # O = Graph()
+    # order = nx.topological_sort(H)
+    # s = order[0]
+    # O.add_node(s, label=G.node[s]['label'])
+    # for t in order[1:]:
+    #     O.add_node(t, label=G.node[t]['label'])
+    #     O.add_edge(s, t)
+    #     s = t
 
     # p = nx.spring_layout(H)
     # nx.draw_networkx(H, p, with_labels=False)
@@ -156,5 +260,6 @@ def test():
     # plt.savefig('/tmp/test.png')
 
     nx.write_dot(H, '/tmp/test.dot')
+    # nx.write_dot(O, '/tmp/testo.dot')
 
 test()
