5af67b93b6b1df67c05dd683d733b7834ba21fc7
Gregor von Laszewski <laszewski@gmail.com>
2016-02-19 10:37:47 -0500

    new:dev: adding web broser to view the svg

--- cloudmesh_workflow/util.py
+++ cloudmesh_workflow/util.py
@@ -8,3 +8,14 @@ def dot2svg(filename, engine='dot'):
     command = "{engine} -Tsvg {file}.dot > {file}.svg".format(**data)
     print(command)
     os.system(command)
+
+
+def browser(filename):
+    data = {
+        'engine': 'python -m webbrowser',
+        'file': filename
+    }
+    if 'file:' not in filename and 'http' not in filename:
+        os.system("python -m webbrowser -t file:///{file}".format(**data))
+    else:
+        os.system("python -m webbrowser -t {file}".format(**data))
