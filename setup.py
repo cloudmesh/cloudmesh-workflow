b7e0bc46781e71d3cecf1b96fe756d6fa071774f
Gregor von Laszewski <laszewski@gmail.com>
2016-03-02 23:41:38 -0500

    fix:dev: fixing the naming convention so that we can install cloudmesh_workflow from other packages such as cloudmesh_portal

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ VERSION = '0.1.0'
 write_version_module(VERSION, 'cloudmesh_workflow/version.py')
 
 setup(
-    name='workflow',
+    name='cloudmesh_workflow',
     version=VERSION,
     packages=find_packages(),
     license='Apache 2.0',
